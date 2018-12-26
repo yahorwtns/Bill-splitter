44a0aea008bc696a51771fd1c87ec1ef258edb32
Yahor <k.minchenok@gmail.com>
2018-12-26 22:43:46 +0300

    html and js syntax modifications

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
 const users = {};
 
-/*create the users object of the following kind:
+/*We’ll store users and their dished in this kind of data structure:
   [guest]:[
     {[dish]:price}
   ]})
 */
 
 const addUser = () => {
-    const userField = document.getElementById("userName");
-    const visitorDrop = document.getElementById("visitorDrop");
-    const option = document.createElement("option");
+  const userField = document.getElementById("userName");
+  const visitorDrop = document.getElementById("visitorDrop");
+  const option = document.createElement("option");
 
-    if (userField.value) {
+  if (userField.value) {
     users[userField.value] = [];
     option.text = userField.value;
     visitorDrop.add(option);
-    refreshTable();
-    userField.value ="";
+      refreshTable();
+      userField.value = "";
     }
 };
 
@@ -25,12 +25,12 @@ const addDish = () => {
   const price = document.getElementById("price");
   const visitorDrop = document.getElementById("visitorDrop");
 
-  users[visitorDrop.value].push({[dishField.value]:price.value});
-    refreshTable();
+  users[visitorDrop.value].push({[dishField.value] : price.value });
+  refreshTable();
   }
 
 //filling the table
-const addRowToTable = (user, price,food) => {
+const addRowToTable = (user, price, food) => {
   const table = document.getElementById("table");
   const footer = document.getElementById("footer");
   const row = table.insertRow(1);
@@ -42,8 +42,8 @@ const addRowToTable = (user, price,food) => {
   cellUser.innerHTML = user;
   cellPrice.innerHTML = price;
   cellFood.innerHTML = food;
-  //calculate total price
-  for(let i = 1, j = table.rows.length; i < j; i++){
+//calculate total price
+  for (let i = 1, j = table.rows.length; i < j; i++) {
       totalSum += parseInt(table.rows[i].cells[1].innerHTML)
   }
   footer.innerHTML = `Сумма чека: ${totalSum}`;
@@ -51,28 +51,31 @@ const addRowToTable = (user, price,food) => {
 //delete all data from the table
 const clearTable = () => {
   const table = document.getElementById("table");
-  let rows = table.rows.length;
-  if(rows>1) {
-  for(let i = 0; i < rows - 1; i++){
-    table.deleteRow(1);
+  const rows = table.rows.length;
+
+  if (rows > 1) {
+    for (let i = 0; i < rows - 1; i++) {
+      table.deleteRow(1);
+    }
   }
-}
 };
 
 const refreshTable= () => {
   clearTable();
   // take all data from the users object
-  let user = Object.keys(users);
-  let price = Object.keys(users)
-  .map(user => users[user]
-  .map(item => Object.values(item)[0])
-  .reduce((a,b) => (+a+ +b),0));
-  let food = Object.keys(users)
-    .map(user => users[user]
-      .map(item => Object.keys(item)[0])
-        .reduce((a,b) => (a +","+ b),""));
+  const user = Object.keys(users);
+  const price = Object.keys(users)
+    .map( user => users[user]
+      .map( item => Object.values(item)[0] )
+        .reduce( (a,b) => (+a + +b) , 0) );
+  const food = Object.keys(users)
+    .map( user => users[user]
+      .map( item => Object.keys(item)[0] )
+        .join(", ") );
+        
 
-    for(let i = 0, j = user.length; i < j ;i++){
-        addRowToTable(user[i],price[i],food[i].substr(1))}
+    for (let i = 0, j = user.length; i < j ;i++) {
+      addRowToTable(user[i], price[i], food[i])
+    }
   
 }
